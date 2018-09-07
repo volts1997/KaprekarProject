aa463c103aa982c54077515cd30800e82d21cf53
volts1997 <42520091+volts1997@users.noreply.github.com>
2018-09-07 08:49:29 +0800

    Line 29
    Change return false; to return 0;

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -1,43 +1,44 @@
 #include<math.h>
-
 #include "kaprekarUtils.h"
 
 /**
  * <p>This function tests whether the given input <code>n</code>
  * is a Kaprekar Number or not.  A Kaprekar Number is a non-negative
- * integer n whose square can be split into two positive integers 
- * whose sum is equal to the original number.  For the purposes of 
+ * integer n whose square can be split into two positive integers
+ * whose sum is equal to the original number.  For the purposes of
  * this method, we restrict our consideration to base-10 representations.</p>
- * 
- * For example, 45 is a Kaprekar number, because 
- *   45<sup>2</sup> = 2025 
- * and 
- *   20 + 25 = 45. 
- * 
- * <p>See Wikipedia's  
+ *
+ * For example, 45 is a Kaprekar number, because
+ *   45<sup>2</sup> = 2025
+ * and
+ *   20 + 25 = 45.
+ *
+ * <p>See Wikipedia's
  * <a href="https://en.wikipedia.org/wiki/Kaprekar_number">Kaprekar Number</a>
  * article for more information.</p>
- * 
+ *
  * @param n
- * @return <code>true</code> if <code>n</code> is a Kaprekar Number, 
+ * @return <code>true</code> if <code>n</code> is a Kaprekar Number,
  * <code>false</code> otherwise.
+
  */
+
 int isKaprekar(int n) {
 
   if(n < 1) {
-    return false;
+    return 0;
   }
 
   int i;
   long square = n * (long) n;
-  int numDigits = (int) log10(n) + 1;
-  long modulus = 0;
+  int numDigits = (int) log10(square) + 1;
+  long modulus = 1;
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
@@ -50,5 +51,5 @@ int isKaprekar(int n) {
     }
   }
   return 0;
-  
+
 }
